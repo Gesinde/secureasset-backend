3891a5a6190da075e4791f8acf11811df1f2d032
Gesinde Abisola <abisolagesinde@gmail.com>
2026-07-28 03:07:06 +0100

    Permission system: migrate transferRoutes.js from checkRole to checkPermission, tested identical behavior

--- routes/transferRoutes.js
+++ routes/transferRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { protect, checkRole } = require('../middleware/auth');
+const { protect, checkPermission } = require('../middleware/auth');
 const { createTransfer, getTransfers, respondToTransfer } = require('../controllers/transferController');
 
 router.use(protect);
-router.post('/', checkRole(['system_admin', 'department_head', 'department_staff']), createTransfer);
+router.post('/', checkPermission('transfer.create'), createTransfer);
 router.get('/', getTransfers);
 router.put('/:id/respond', respondToTransfer);
 
