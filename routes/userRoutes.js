a58acfa6f7614560a95c4d8355c7fbc71bedf3ee
Gesinde Abisola <abisolagesinde@gmail.com>
2026-07-28 02:39:24 +0100

    Permission system: migrate userRoutes.js from checkRole to checkPermission, tested identical behavior

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { protect, checkRole } = require('../middleware/auth');
+const { protect, checkPermission } = require('../middleware/auth');
 const { getUsers, createUser, updateUser, setUserActiveStatus } = require('../controllers/userController');
 
 router.use(protect);
 router.get('/', getUsers);
-router.post('/', checkRole(['system_admin']), createUser);
-router.put('/:id', checkRole(['system_admin']), updateUser);
-router.put('/:id/status', checkRole(['system_admin']), setUserActiveStatus);
+router.post('/', checkPermission('user.create'), createUser);
+router.put('/:id', checkPermission('user.update'), updateUser);
+router.put('/:id/status', checkPermission('user.status'), setUserActiveStatus);
 
 module.exports = router;
