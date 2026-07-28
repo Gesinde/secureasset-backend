3f13a79f9e688166563a7e114cbce4293d34a37d
Gesinde Abisola <abisolagesinde@gmail.com>
2026-07-28 10:59:33 +0100

    Permission system: migrate auditRoutes.js and adminRoutes.js from checkRole to checkPermission, tested identical behavior

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { protect, checkRole } = require('../middleware/auth');
+const { protect, checkPermission } = require('../middleware/auth');
 const { getStats } = require('../controllers/adminController');
 
 router.use(protect);
-router.get('/stats', checkRole(['system_admin']), getStats);
+router.get('/stats', checkPermission('admin.stats'), getStats);
 
 module.exports = router;
--- routes/auditRoutes.js
+++ routes/auditRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { protect, checkRole } = require('../middleware/auth');
+const { protect, checkPermission } = require('../middleware/auth');
 const { getAuditLogs } = require('../controllers/auditController');
 
 router.use(protect);
-router.get('/', checkRole(['system_admin', 'auditor']), getAuditLogs);
+router.get('/', checkPermission('audit.view'), getAuditLogs);
 
 module.exports = router;
